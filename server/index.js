7f7e495c8d262da16e4e0a9ef29b732a2854f6ee
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2022-03-10 16:27:54 -0500

    comment

--- server/index.js
+++ server/index.js
@@ -1,23 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3456;
-const router = require('./router');
-const cors = require('cors');
+const router = require("./router");
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json()); //body parser
 app.use(router);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   try {
-    res.send('server is connected!')
+    res.send("server is connected!");
   } catch {
-    res.send('server failed to connect')
-    res.status(404)
+    res.send("server failed to connect");
+    res.status(404);
   }
 });
 
 app.listen(port, () => {
-  console.log(`I'm listening on port ${port}`)
-})
-
+  console.log(`I'm listening on port ${port}`);
+});
