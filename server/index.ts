4f66e627ddc29980ab8318cc1f8c9087138f2486
Rudy451 <dane.johnson451@gmail.com>
2022-03-11 07:15:34 -0800

    finish server changes

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,14 @@
-const express = require('express');
-const app = express();
+const mainExpress = require('express');
+const app = mainExpress();
 const port = 3456;
-const router = require('./router');
+const mainRouter = require('./router');
 const cors = require('cors');
 
 app.use(cors());
-app.use(express.json()); //body parser
-app.use(router);
+app.use(mainExpress.json()); //body parser
+app.use(mainRouter);
 
-app.get('/', (req, res) => {
+app.get('/', (req:any, res:any) => {
   try {
     res.send('server is connected!')
   } catch {
@@ -20,4 +20,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`I'm listening on port ${port}`)
 })
-
---
+++ server/models.ts
@@ -0,0 +1,63 @@
+import {Schema, model, connect, connection} from "mongoose";
+
+connect('mongodb://localhost:27017/wipDB');
+
+connection.once('open', function () {
+  console.log('MongoDB database connection established successfully');
+});
+
+interface commentInterface {
+  comment?: string,
+  upload_date?: string,
+  seen_by_state?: string,
+  seen_by_user?: string
+}
+
+const commentSchema = new Schema({
+  comment: String,
+  upload_date: String,
+  seen_by_state: String,
+  seen_by_user: String,
+});
+
+const commentList = model<commentInterface>('Comment', commentSchema);
+
+interface cardInterface {
+  img_url?: String;
+  upload_date?: String;
+  seen_by_state?: String;
+  seen_by_user?: String;
+  seen_by_date?: String;
+  comments?: [typeof commentSchema];
+  wipId?: Schema.Types.ObjectId
+}
+
+const cardSchema = new Schema<cardInterface>({
+  img_url: String,
+  upload_date: String,
+  seen_by_state: String,
+  seen_by_user: String,
+  seen_by_date: String,
+  comments: [commentSchema],
+  wipId: Schema.Types.ObjectId
+});
+
+const cardList = model<cardInterface>('Card', cardSchema);
+
+interface wipInterface {
+  wip_title: string;
+  wip_cards?: [typeof cardSchema];
+  update_request?: string;
+  update_request_date?: string;
+}
+
+const wipSchema = new Schema<wipInterface>({
+  wip_title: {type: String, required: true},
+  wip_cards: [cardSchema],
+  update_request: String,
+  update_request_date: String,
+});
+
+export const wipList = model<wipInterface>('Wip', wipSchema);
+
+module.exports = {wipList, cardList, commentList}
