31450f5ca291fdaed1fca23437b52021d859e89e
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2022-03-14 22:30:50 -0400

    card form

--- client/wip-app/src/userContext.jsx
+++ client/wip-app/src/userContext.jsx
@@ -2,3 +2,4 @@ import { createContext } from "react";
 
 export const UserContext = createContext(null);
 export const CollectionContext = createContext(null);
+export const WipContext = createContext(null);
