39aefda73f879f025993664492f654007da6ec74
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2022-03-15 15:22:49 -0400

    testing

--- server/router.ts
+++ server/router.ts
@@ -1,23 +1,22 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
-const methods = require('./controllers');
+const methods = require("./controllers");
 
-router.post('/register', methods.registerUser);
-router.post('/login', methods.loginUser);
-router.get('/wips', methods.getWips);
-router.get('/cards', methods.getAllCards);
-router.get('/comments', methods.getAllComments);
-router.get('/wip/:wipId/cards', methods.getCards);
-router.post('/wips', methods.addWip);
-router.put('/wips/addComment/:cardId', methods.addComment);
-router.post('/wips/:wipId', methods.addCard);
-router.delete('/wips/:wipId', methods.deleteWip);
-router.delete('/wips/:wipId/card/:cardId', methods.deleteCard);
-router.patch('/wips/updateRequest/:wipId', methods.updateRequest);
-router.patch('/wips/updateTitle/:wipId', methods.updateTitle);
-router.patch('/wips/updateCard/:wipId/:cardId', methods.updateCard);
+router.post("/register", methods.registerUser);
+router.post("/login", methods.loginUser);
+router.get("/wips", methods.getWips);
+router.get("/cards", methods.getAllCards);
+router.get("/comments", methods.getAllComments);
+router.get("/wip/:wipId/cards", methods.getCards);
+router.post("/wips", methods.addWip);
+router.put("/wips/addComment/:cardId", methods.addComment);
+router.post("/wips/:wipId", methods.addCard);
+router.delete("/wips/:wipId", methods.deleteWip);
+router.delete("/wips/:wipId/card/:cardId", methods.deleteCard);
+router.patch("/wips/updateRequest/:wipId", methods.updateRequest);
+router.patch("/wips/updateTitle/:wipId", methods.updateTitle);
+router.patch("/wips/updateCard/:wipId/:cardId", methods.updateCard);
 // router.patch('/wips/updateComment/:wipId/:cardId/:commentId', methods.updateComment);
-
 
 module.exports = router;
