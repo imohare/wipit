5875746d80a3b8eba118fe9b8174f7b145d8f5ac
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2022-03-16 13:30:22 -0400

    staged changes

--- client/wip-app/src/screens/ArtistProfile.tsx
+++ client/wip-app/src/screens/ArtistProfile.tsx
@@ -4,14 +4,30 @@ import { Box, Button, Container, Flex, Text, Wrap } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import ArtistCollectionForm from "../components/ArtistCollectionForm";
 import ArtistCardForm from "../components/ArtistCardForm";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CollectionContext, UserContext } from "../userContext";
 import WipItem from "../components/wipItem";
 import { userInfo } from "os";
+import methods from "../services";
 const nftBanner = require("../assets/nft.png");
 
 function ArtistProfile(): JSX.Element {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+
+  // async const userFunction = ()=> {
+  //   const userInfo = await methods.getUser(userLogin);
+  //   let path = "";
+  //   console.log("user created: ", userInfo);
+  //   if (userInfo) {
+  //     setUser(userInfo);
+  //     path = path.concat(
+  //       userInfo.type == "artist"
+  //         ? `/a/${userInfo.profileId}`
+  //         : `/g/${userInfo.profileId}`
+  //     );
+  // }
+
+  useEffect(() => {}, []);
   return (
     <>
       {/* <ArtistWipsButton /> */}
--- server/models/index.ts
+++ server/models/index.ts
@@ -1,27 +1,27 @@
-import Sequelize from '@sequelize/core';
-import sequelize from './config';
-import Login from './userLogin';
-import Profile from './userProfile';
-import WipCollections from './wipcollections';
-import Wips from './wips';
-import Followers from './followers';
+import Sequelize from "@sequelize/core";
+import sequelize from "./config";
+import Login from "./userLogin";
+import Profile from "./userProfile";
+import WipCollections from "./wipcollections";
+import Wips from "./wips";
+import Followers from "./followers";
 
-Login.belongsTo(Profile, {foreignKey: 'profileId'});
-Profile.hasOne(Login, {foreignKey: 'profileId'});
-WipCollections.belongsTo(Profile, {foreignKey: 'profileId'});
-Profile.hasMany(WipCollections, {foreignKey: 'profileId'});
-Wips.belongsTo(WipCollections, {foreignKey: 'wipCollectionId'});
-WipCollections.hasMany(Wips, {foreignKey: 'wipCollectionId'});
-Profile.hasOne(Followers, {foreignKey: 'profileId'});
-Followers.belongsTo(Profile, {foreignKey: 'userId'});
-Followers.belongsTo(Profile, {foreignKey: 'followerId'});
+Login.belongsTo(Profile, { foreignKey: "profileId" });
+Profile.hasOne(Login, { foreignKey: "profileId" });
+WipCollections.belongsTo(Profile, { foreignKey: "profileId" });
+Profile.hasMany(WipCollections, { foreignKey: "profileId" });
+Wips.belongsTo(WipCollections, { foreignKey: "wipCollectionId" });
+WipCollections.hasMany(Wips, { foreignKey: "wipCollectionId" });
+Profile.hasOne(Followers, { foreignKey: "profileId" });
+Followers.belongsTo(Profile, { foreignKey: "userId" });
+Followers.belongsTo(Profile, { foreignKey: "followerId" });
 
 const db = {
-  'Login': Login,
-  'Profile': Profile,
-  'WipCollections': WipCollections,
-  'Wips': Wips,
-  'Followers': Followers,
+  Login: Login,
+  Profile: Profile,
+  WipCollections: WipCollections,
+  Wips: Wips,
+  Followers: Followers,
   Sequelize: Sequelize,
   sequelize: sequelize,
 };
