1792b35cd1e7dfcf3acea894f2f325b842f046a7
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2022-04-04 14:36:26 -0400

    sav

--- client/wip-app/src/components/wipItem.jsx
+++ client/wip-app/src/components/wipItem.jsx
@@ -1,6 +1,6 @@
-import { Box, Image, Text } from '@chakra-ui/react';
-import React, { useContext } from 'react';
-import { WipContext } from '../userContext';
+import { Box, Image, Text } from "@chakra-ui/react";
+import React, { useContext } from "react";
+import { WipContext } from "../userContext";
 
 function WipItem() {
   const { wip } = useContext(WipContext);
@@ -8,30 +8,18 @@ function WipItem() {
   return (
     <Box
       ml={10}
-<<<<<<< HEAD
-      w="350px"
+      w="400px"
       h="250px"
       boxShadow="lg"
       rounded="7px"
       bg={"whiteAlpha.100"}
       overflow="hidden"
-=======
-      w='400px'
-      h='250px'
-      boxShadow='lg'
-      rounded='7px'
-      bg={'whiteAlpha.100'}
-      overflow='hidden'
->>>>>>> ee3bb7e508a268a981f82cf8cea218875b8633d3
     >
       <Image
-        src={wip ? wip[wip.length - 1].wipImage : 'None'}
-        alt='user Image'
+        src={wip ? wip[wip.length - 1].wipImage : "None"}
+        alt="user Image"
       />
-<<<<<<< HEAD
-=======
       <Text>{wip?.wipTitle}</Text>
->>>>>>> ee3bb7e508a268a981f82cf8cea218875b8633d3
     </Box>
   );
 }
